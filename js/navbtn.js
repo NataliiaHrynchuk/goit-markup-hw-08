77d0843dd0f00ab36eb996ab078fe91a0c639994
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2021-12-17 17:38:58 +0200

    Update navbtn.js

--- js/navbtn.js
+++ js/navbtn.js
@@ -1,8 +1,15 @@
 (() => {
   const navBtnRef = document.querySelector('[data-nav-button]');
+  const mobilemenuRef = document.querySelector('[data-menu]');
 
-navBtnRef.addEventListener('click', () => {
+  navBtnRef.addEventListener('click', () => {
+    const expanded =
+      navBtnRef.getAttribute("aria-expanded") === "true" || false;
   
- navBtnRef.classList.toggle('is-open');
+    navBtnRef.classList.toggle('is-open');
+    navBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobilemenuRef.classList.toggle('is-open');
+
 });
 })();

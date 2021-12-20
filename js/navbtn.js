80d572c20324018fa1587b858f38aabf9de62375
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2021-12-20 22:30:44 +0200

    Update navbtn.js

--- js/navbtn.js
+++ js/navbtn.js
@@ -2,13 +2,13 @@
   const refs = {
     openNavBtn: document.querySelector('.nav__open'),
     closeNavBtn: document.querySelector('.nav__close'),
-    nuv: document.querySelector('.nav__container'),
+    modal: document.querySelector('.nav__container'),
   };
 
   refs.openNavBtn.addEventListener('click', toggleNav);
   refs.closeNavBtn.addEventListener('click', toggleNav);
 
   function toggleNav() {
-    refs.nav.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
